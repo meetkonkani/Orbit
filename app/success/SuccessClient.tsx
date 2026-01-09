ffc5386f1983c211aac96d6cff1cea4591429368
Meet Konkani <meetkonkani@gmail.com>
2026-01-09 13:43:08 +0530

    Fix useSearchParams suspense error on success page

---
+++ app/success/SuccessClient.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import SuccessClient from "./SuccessClient";
+
+export default function SuccessPage() {
+  return (
+    <Suspense fallback={<div className="p-10 text-center">Loading...</div>}>
+      <SuccessClient />
+    </Suspense>
+  );
+}
